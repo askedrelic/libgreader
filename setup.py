8d1a5f9a7376268f19d681265f42a60ce04257d8
Matt Behrens <askedrelic@gmail.com>
2010-12-29 22:20:09 -0500

    Woohoo, moving project files to be a legit Python project
    Now 0.4 available at http://pypi.python.org/pypi/libgreader/

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='libgreader',
+      version = '0.4',
+      author='askedrelic',
+      author_email='askedrelic@gmail.com',
+      packages = find_packages(),
+)
