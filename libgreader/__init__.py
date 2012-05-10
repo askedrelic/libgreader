36f1d08b05293d2c41a45eaf0458e628f1353919
Matt Behrens <askedrelic@gmail.com>
2012-05-09 21:11:25 -0700

    simplify. 0.6.0 is what version I'm working toward

--- libgreader/__init__.py
+++ libgreader/__init__.py
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
 
-"""
-libG(oogle)Reader
-Copyright (C) 2010  Matt Behrens <askedrelic@gmail.com> http://asktherelic.com
-
-Python library for working with the unofficial Google Reader API.
-Google may break this at anytime, I am not responsible for damages from that
-breakage, but I will try my best to fix it.
-
-Uses HTTPS for all requests to and from Google.
-
-Licensing included in LICENSE.txt
-"""
+# libgreader
+# Copyright (C) 2012  Matt Behrens <askedrelic@gmail.com>
+# Python library for the Google Reader API
 
 __author__  = "Matt Behrens <askedrelic@gmail.com>"
-__version__ = "0.6.0beta1"
-__credits__ = "Matt Behrens <askedrelic@gmail.com>, Stephane Angel aka Twidi <s.angel@twidi.com>"
+__version__ = "0.6.0"
+__copyright__ = "Copyright (C) 2012  Matt Behrens"
 
 from googlereader import GoogleReader
 from auth import AuthenticationMethod, ClientAuthMethod, OAuthMethod, OAuth2Method
