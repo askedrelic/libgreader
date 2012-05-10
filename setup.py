67a8d6e7104ec8517db63d5ad18a422deea6b7ed
Matt Behrens <askedrelic@gmail.com>
2012-05-09 21:18:17 -0700

    more cleanup

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 import libgreader
 
 setup(
-name         = 'libgreader',
-version      = libgreader.__version__,
-author       = libgreader.__author__,
-author_email = 'askedrelic@gmail.com',
+    name             = 'libgreader',
+    version          = libgreader.__version__,
+    description      = 'Library for working with the Google Reader API',
+    long_description = open('README.md').read() + '\n\n' + open('HISTORY.md').read(),
 
-description      = 'Library for working with the Google Reader API',
-long_description = open('README.md').read(),
+    author           = libgreader.__author__,
+    author_email     = 'askedrelic@gmail.com',
+    url              = 'https://github.com/askedrelic/libgreader',
+    license          = open("LICENSE.txt").read(),
 
-url          = 'https://github.com/askedrelic/libgreader',
-# Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-#classifiers  = ['Development Status :: 3 - Alpha', ],
-
-packages     = find_packages(),
-test_suite   = 'tests',
+    packages         = find_packages(),
+    test_suite       = 'tests',
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    #classifiers  = ['Development Status :: 3 - Alpha', ],
 )
