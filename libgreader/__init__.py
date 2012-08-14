3ebf329cde464e8e0bd9bd32ba1a91ff2dfaf93f
Matt Behrens <askedrelic@gmail.com>
2012-08-13 22:07:51 -0700

    lets start work on the next version

--- libgreader/__init__.py
+++ libgreader/__init__.py
@@ -5,7 +5,7 @@
 # Python library for the Google Reader API
 
 __author__  = "Matt Behrens <askedrelic@gmail.com>"
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __copyright__ = "Copyright (C) 2012  Matt Behrens"
 
 from googlereader import GoogleReader
