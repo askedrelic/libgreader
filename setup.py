0b78d37dfbc99b6e13b104fbb0a63e633865c92f
Matt Behrens <askedrelic@gmail.com>
2013-03-18 23:29:49 -0700

    release 0.7!

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ),
